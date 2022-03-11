d68f9e7448f2d1274ea78853ad73446bc5144b82
SlyFryFrog <78104059+SlyFryFrog@users.noreply.github.com>
2022-03-11 18:58:18 -0500

    Update password.py

--- password.py
+++ password.py
@@ -2,11 +2,11 @@ import secrets
 import string
 
 password = []
-temp = ''
 get_token = False
 
 # Variables of available characters for the password
 characters = list(string.ascii_letters + string.digits + string.punctuation)
+
 
 # Sets default variables and prompts user for input
 def setup(password, get_token):
@@ -14,18 +14,18 @@ def setup(password, get_token):
     
     # Checks for first time launch
     if not get_token:
-        repeat = int(input('How many characters long do you want your password? '))
+        password_length = int(input('How many characters long do you want your password? '))
 
-        return main(x, repeat, password)
+        return main(password_length, password)
     
     # Prompts the user if they would like to generate another password
     else:
         again = input('\nWould you like to create another password? y/n: ')
 
         if again.lower() == 'y':
-            repeat = int(input('\nHow many characters long do you want your password? '))
+            password_length = int(input('\nHow many characters long do you want your password? '))
 
-            return main(x, repeat, password)
+            return main(password_length, password)
 
         else:
             exit
@@ -33,27 +33,17 @@ def setup(password, get_token):
 
 # Repeats password_gen(x, repeat) until it's reached the set characters
 # After, it joins all the characters in the list into a singular string
-def main(x, repeat, password):
-
-    if x < repeat:
-        x = x + 1
-
-        return append_list(temp, x, repeat, password)
+def main(passwprd_length, password):
+    for i in range(passwprd_length):
+        temp = secrets.choice(characters)
+        password.append(temp)
+    
 
     # Prints the password and returns to setup()
-    else:
-        print(''.join(password))
-        password = []
-        get_token = True
+    print(''.join(password))
+    password = []
+    get_token = True
 
-        return setup(password,get_token)
-
-
-# Adds the temp variable to the password list
-def append_list(temp, x, repeat, password):
-    temp = secrets.choice(characters)
-    password.append(temp)
-
-    return main(x, repeat, password)
+    return setup(password,get_token)
 
 setup(password, get_token)
