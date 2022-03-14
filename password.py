f1eecf2036086d818bc8c93b96603009f6f8e055
SlyFryFrog <78104059+SlyFryFrog@users.noreply.github.com>
2022-03-14 18:25:07 -0400

    Update password.py

--- password.py
+++ password.py
@@ -10,15 +10,13 @@ characters = list(string.ascii_letters + string.digits + string.punctuation)
 
 # Sets default variables and prompts user for input
 def setup(password, get_token):
-    x = 0
-    
     # Checks for first time launch
     if not get_token:
         print('Welcome to the Password Generator made by SlyFryFrog\n')
         password_length = int(input('Please enter the desired length of your password: '))
 
         return main(password_length, password)
-    
+
     # Prompts the user if they would like to generate another password
     else:
         again = input('\nWould you like to create another password? y/n: ')
@@ -28,31 +26,28 @@ def setup(password, get_token):
 
             return main(password_length, password)
 
-        else:
-            exit
-
 
 # Repeats until it's reached the set characters
 # After, it joins all the characters in the list into a singular string
 def main(password_length, password):
-
     # Checks if user input is too short
     if password_length < 8:
-        print(f'Sorry, but {password_length} is too short. Please enter a value greater than 7 to create a more secure password.')
+        print(f'Sorry, but {password_length} is too short.')
+        print('Please enter a value greater than 7 to create a more secure password.')
         get_token = True
         return setup(password, get_token)
 
-    else: 
+    else:
         for i in range(password_length):
             temp = secrets.choice(characters)
             password.append(temp)
-        
 
-        # Prints the password and returns to setup()
+        # Prints the password and returns to set up()
         print(''.join(password))
         password = []
         get_token = True
 
-        return setup(password,get_token)
+        return setup(password, get_token)
+
 
 setup(password, get_token)
